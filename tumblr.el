d49a6afbabf959e7a2d83d82ddf0dc5fbb084996
Dan Leslie <dleslie@marvin.(none)>
2010-02-27 21:03:35 -0800

    Fixed a slew of bugs, segmented into two separate request functions

--- tumblr.el
+++ tumblr.el
@@ -9,22 +9,31 @@
   (gist-region begin end private 'tumblr-gist-callback))
 
 (defun tumblr-gist-callback (status)
-  (with-current-buffer (url-retrieve-synchronously (cadr status))
-    (destructuring-bind (email . password) (tumblr-auth-info)
-      (let* ((title (read-string "Post title: "))
-             (data (replace-regexp-in-string 
-                    "\\(<pre>\\|</pre>\\)" ""
-                    (buffer-substring (point-min) (point-max))))
-             (url-request-method "POST")
-             (url-request-extra-headers '(("Content-Type" . "application/xml")))
-             (url-request-data (gist-make-query-string
-                                '(("email" . email)
-                                ("password" . password)
-                                ("title" . title)
-                                ("format" . "html")
-                                ("private" . "0")
-                                ("body" . data)))))
-        (kill-buffer (url-retrieve-synchronously "http://www.tumblr.com/api/write"))))))
+  (let* ((url-max-redirections 5)
+         (url-request-method "GET")
+         (gist-buffer (url-retrieve-synchronously (cadr status))))
+    (with-current-buffer gist-buffer
+      (let* ((content (buffer-substring (point-min) (point-max)))
+             (fixed-content (replace-regexp-in-string 
+                             "\\(<pre>\\|</pre>\\)" ""
+                             content))
+             (title (read-string "Post title: ")))
+        (tumblr-post-data title content)))))
+
+(defun tumblr-post-data (title data)
+  (destructuring-bind (email . password) (tumblr-auth-info)
+    (let ((url-max-redirections 5)
+          (url-request-method "POST")
+          (url-request-extra-headers '(("Content-Type" . "application/xml")))
+          (url-request-data (gist-make-query-string
+                             `(("email" . ,email)
+                               ("password" . ,password)
+                               ("title" . ,title)
+                               ("format" . "html")
+                               ("private" . "0")
+                               ("body" . ,data)))))
+      (url-retrieve "http://www.tumblr.com/api/write" 
+                    (lambda (status) (kill-buffer (current-buffer)))))))
 
 (defun tumblr-auth-info ()
   "Returns the user's tumblr authorization information."
@@ -32,7 +41,7 @@
   (when (not tumblr-email)
     (setq tumblr-email (read-string "Tumblr email: ")))
   (when (not tumblr-password)
-    (setq tumblr-password (read-string "Tumblr password: ")))
+    (setq tumblr-password (read-passwd "Tumblr password: ")))
   (cons tumblr-email tumblr-password))
 
 (provide 'tumblr)
